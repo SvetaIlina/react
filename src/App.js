940afbc7f0450c26c59590b5a8511df5539231fa
Svetlana <svetailina1987@gmail.com>
2023-10-12 19:24:34 +0400

    component and StrictMode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {StrictMode} from 'react';
 import './App.css';
+
+
+const Header = () => {
+  return <h2>Hello World!</h2>
+}
+
+// const Field = () => {
+//   const holder = 'Enter here'
+//   const styleField = {
+//       width: '300px'
+//     }
+//   return <input 
+//     type="text" 
+//     placeholder={holder} 
+//     style={styleField}/>
+// }
+
+class Field extends React.Component{
+  render() {
+    const holder = 'Enter here'
+    const styleField = {
+      width: '300px'
+    }
+    return <input 
+              type="text" 
+              placeholder={holder} 
+              style={styleField}/>
+  }
+}
+
+function Btn() {
+  const text = 'Log in'
+  const logged = true
+  // const p = <p>LOG IN</p>
+  // const res = () => {
+  //   return 'Log in'
+  // }
+  return <button>{logged ? 'Enter' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StrictMode>
+        <Header />
+      </StrictMode>
+      <Field />
+      <Btn />
     </div>
   );
 }
-
+export {Header};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import { Header } from './App';
 
-const text = 'Hello WorlD'
-const elem = (
-  <div>
-    <h2 className='header'>Text: {text}</h2>
-    <label htmlFor="iii">Input</label>
-    <input type='text' name='iii' />
-    <button tabIndex={0}>{2+2}</button>
-  </div>
-);
-
-// const elem = React.createElement('h2', {className: 'header'}, 'Hello WorlD!');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <StrictMode>
+    <App/>
+  </StrictMode>
 );
 
